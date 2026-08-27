2c9db2e7c880d04a5237e9e6ff7a6160004fbbb7
Joseph Taylor <taylorjoseph3333@gmail.com>
2026-08-27 15:55:27 +0100

    WIP

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from python.src.solver import solve_puzzle
 from python.src.generator import generate_puzzle
 
-puzzle = generate_puzzle()
-print(puzzle.numbers, puzzle.target)
-result = solve_puzzle(puzzle)
-if result:
-    for calculation in result.calculations:
-        print(calculation.__str__())
-else:
-    print("No solution found")
+def generate_and_solve_puzzle():
+    puzzle = generate_puzzle()
+    print(puzzle.numbers, puzzle.target)
+    result = solve_puzzle(puzzle)
+    if result:
+        for calculation in result.calculations:
+            print(calculation.__str__())
+    else:
+        print("No solution found")
+
+if __name__ == "__main__":
+    generate_and_solve_puzzle()
