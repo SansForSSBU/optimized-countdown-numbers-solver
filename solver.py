fd7d60e89bdc7541beda0bc1c8a4aacee8b3a807
Joseph Taylor <taylorjoseph3333@gmail.com>
2026-08-27 14:49:00 +0100

    Added solution printing

--- solver.py
+++ solver.py
@@ -1,28 +1,69 @@
 from copy import deepcopy
 import operator
 operators = [operator.add, operator.sub, operator.mul, operator.floordiv]
+op_str_lookup = {
+    operator.add: "+",
+    operator.sub: "-",
+    operator.mul: "*",
+    operator.floordiv: "/"
+}
 
-def solve_puzzle(numbers, target):
-    state = numbers
-    for idx1, n1 in enumerate(state):
-        for idx2, n2 in enumerate(state):
+class Calculation:
+    def __init__(self, n1, op, n2):
+        self.n1 = n1
+        self.op = op
+        self.n2 = n2
+
+    def result(self):
+        if self.op == operator.floordiv:
+            if self.n1 % self.n2 != 0:
+                return None
+        ans = self.op(self.n1, self.n2)
+        if ans <= 0:
+            return None
+        return ans
+
+    def __str__(self):
+        return f"{self.n1} {op_str_lookup[self.op]} {self.n2} = {self.result()}"
+
+    def __repr__(self):
+        return self.__str__()
+
+class State:
+    def __init__(self, numbers, target, calculations):
+        self.numbers = numbers
+        self.target = target
+        self.calculations = calculations
+
+
+def solve_puzzle(state):
+    for idx1, n1 in enumerate(state.numbers):
+        for idx2, n2 in enumerate(state.numbers):
             if idx1 == idx2:
                 continue
             for op in operators:
-                if op == operator.floordiv:
-                    if n1 % n2 != 0:
-                        continue
-                new_num = op(n1, n2)
-                if new_num <= 0:
+                calc = Calculation(n1, op, n2)
+                new_num = calc.result()
+                if new_num is None:
                     continue
+                new_numbers = [n for idx, n in enumerate(state.numbers) if idx not in [idx1, idx2]]
+                new_numbers.append(new_num)
+                new_calculations = deepcopy(state.calculations)
+                new_calculations.append(calc)
+                new_state = State(new_numbers, state.target, new_calculations)
                 if new_num == target:
-                    return True
-                new_state = [n for idx, n in enumerate(state) if idx not in [idx1, idx2]]
-                new_state.append(new_num)
-                if solve_puzzle(new_state, target):
-                    return True
+                    return new_state
+                result = solve_puzzle(new_state)
+                if result:
+                    return result
     return False
 
 numbers = [1, 2, 3, 4, 5, 6]
 target = 200
-print(solve_puzzle(numbers, target))
+state = State(numbers, target, [])
+result = solve_puzzle(state)
+if result:
+    for calculation in result.calculations:
+        print(calculation.__str__())
+else:
+    print("No solution found")
