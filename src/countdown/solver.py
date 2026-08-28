b0b1e8d2489d0c1164e80689285c27b15f7dff7e
Joseph Taylor <taylorjoseph3333@gmail.com>
2026-08-28 18:08:14 +0100

    WIP

--- src/countdown/solver.py
+++ src/countdown/solver.py
@@ -14,7 +14,7 @@ def solve_puzzle(state, curr_best_num=None):
     best_state = state
     best_steps = ()
     if state.target in state.numbers:
-        return (state, ())
+        return (state.target, ())
     for op_idx, op in enumerate(operators):
         if op in commutative_operators:
             space = itertools.combinations(enumerate(state.numbers), 2)
@@ -22,19 +22,16 @@ def solve_puzzle(state, curr_best_num=None):
             space = itertools.permutations(enumerate(state.numbers), 2)
         for (idx1, n1), (idx2, n2) in space:
             new_num = calculate(n1, op_idx, n2)
-            step = encode_calculation(idx1, op_idx, idx2)
             if new_num is None:
                 continue
             new_numbers = [n for idx, n in enumerate(state.numbers) if idx != idx1 and idx != idx2]
             new_numbers.append(new_num)
-            new_state = State(new_numbers, state.target)
-            new_state.recompute_best()
             if new_num == state.target:
-                return (new_state, (step,))
-            (result, steps) = solve_puzzle(new_state, curr_best_num=best_num)
-            if result.best == result.target:
-                return (result, (step,) + steps)
-            if abs(result.target - result.best) < abs(best_state.target - best_state.best):
-                best_state = result
-                best_steps = (step,) + steps
-    return (best_state, best_steps)
+                return (new_num, (encode_calculation(idx1, op_idx, idx2),))
+            (result_best_num, steps) = solve_puzzle(State(new_numbers, state.target), curr_best_num=best_num)
+            if result_best_num == state.target:
+                return (result_best_num, (encode_calculation(idx1, op_idx, idx2),) + steps)
+            if abs(state.target - result_best_num) < abs(best_state.target - best_state.best):
+                best_num = result_best_num
+                best_steps = (encode_calculation(idx1, op_idx, idx2),) + steps
+    return (best_num, best_steps)
